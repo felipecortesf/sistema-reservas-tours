e5489f262d00051c9f339ac08ddb5c2f4d005866
felipecortesf <feli.cortesf@duocuc.cl>
2026-05-06 14:11:46 -0400

    feat: agregar ms-catalogo-tours con 16 tours de Rio de Janeiro puerto 8082

---
+++ ms-catalogo-tours/src/test/java/com/reservatours/ms_catalogo_tours/MsCatalogoToursApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reservatours.ms_catalogo_tours;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCatalogoToursApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
