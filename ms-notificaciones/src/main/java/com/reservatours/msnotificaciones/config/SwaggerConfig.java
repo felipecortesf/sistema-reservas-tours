b058b79d3fdf755c0df67650c47d39bc9134aab1
felipecortesf <feli.cortesf@duocuc.cl>
2026-05-07 18:02:44 -0400

    feat: agregar Swagger SpringDoc en todos los microservicios

---
+++ ms-notificaciones/src/main/java/com/reservatours/msnotificaciones/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.reservatours.msnotificaciones.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("ms-notificaciones API")
+                        .version("1.0.0")
+                        .description("API del microservicio ms-notificaciones - Sistema de Reservas de Tours Rio de Janeiro"));
+    }
+}
