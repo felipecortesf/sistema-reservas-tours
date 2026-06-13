e499b7a1a78a67d8f351a56c30171d6866acc453
felipecortesf <feli.cortesf@duocuc.cl>
2026-06-13 10:39:52 -0400

    feat: Spring Security + JWT + RBAC en los 7 microservicios restantes

---
+++ ms-embarques/src/main/java/com/reservatours/msembarques/config/JwtUtil.java
@@ -0,0 +1,33 @@
+package com.reservatours.msembarques.config;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+import javax.crypto.SecretKey;
+
+@Component
+public class JwtUtil {
+
+    private final SecretKey key = Keys.hmacShaKeyFor(
+        "ReservaToursSecretKeyParaJWT2026MuySeguraYLarga".getBytes()
+    );
+
+    public String getEmailFromToken(String token) {
+        return Jwts.parserBuilder().setSigningKey(key).build()
+                .parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public String getRolFromToken(String token) {
+        return (String) Jwts.parserBuilder().setSigningKey(key).build()
+                .parseClaimsJws(token).getBody().get("rol");
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+}
